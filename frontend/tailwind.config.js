61529da7239f60f01b165bc69dbcb04b7c0b4e11
Jake Ball <jakeb@galil.com>
2023-06-21 13:23:27 -0700

    Added new fonts and set default colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'Arial', 'sans-serif'],
+      },
+      colors: {
+        primary: '#E52760',
+        secondary: '#DC2626',
+        background: '#FFD6E8',
+      },
+    },
+  },
   plugins: [],
 }
